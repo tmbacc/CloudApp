b1b5db2848b8e76a9f878486cd4a67574f8142c9
tmbacc <tmqh@163.com>
2020-02-17 22:38:58 +0800

    update Application.java

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //启动代码
+        //启动代码！！！！
     	SpringApplication.run(Application.class, args);
         
         
